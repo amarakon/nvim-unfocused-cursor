28f0194d59b3c60164384ac559179a8017569ed7
amarakon <amar.al-zubaidi45@tutanota.com>
2022-11-03 11:45:34 -0400

    Only use local variables

--- lua/unfocused-cursor.lua
+++ lua/unfocused-cursor.lua
@@ -10,19 +10,23 @@ local o = vim.o
 local opt = vim.opt
 
 function main.setup()
+	local old_guicursor,old_cursorline,old_cursorcolumn
 	autocmd("VimEnter", function ()
 		old_guicursor = o.guicursor
 		old_cursorline = o.cursorline
+		old_cursorcolumn = o.cursorcolumn
 	end)
 
 	autocmd({ "WinLeave", "FocusLost" }, function ()
 		opt.guicursor = "a:noCursor"
 		opt.cursorline = false
+		opt.cursorcolumn = false
 	end)
 
 	autocmd({ "WinEnter", "FocusGained" }, function ()
 		opt.guicursor = old_guicursor
 		opt.cursorline = old_cursorline
+		opt.cursorcolumn = old_cursorcolumn
 	end)
 end
 
